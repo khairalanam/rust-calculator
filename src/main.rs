1ef1ff55a3be02c7835620a042575e48475afe25
Khair Alanam <anaambasheer@gmail.com>
2023-09-12 12:58:11 +0530

    refactor code

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use std::io;
 
 fn main() {
     let result: f64;
-    let mut op: String = String::new();
 
     println!("Enter the first number: ");
     let x: f64 = input_parser();
@@ -28,14 +27,19 @@ fn main() {
     println!("(4) Divide");
     println!("Select the number associated with the desired operation: ");
 
-    io::stdin().read_line(&mut op).expect("Invalid Input");
-    let op: i32 = match op.trim().parse() {
-        Ok(num) => num,
-        Err(_) => {
-            println!("Invalid input!");
-            return;
-        }
-    };
+    let op: f64 = input_parser();
+
+    if f64::is_nan(op) {
+        println!("Invalid input!");
+        return;
+    }
+
+    let op: i32 = op as i32;
+
+    if op > 4 || op < 1 {
+        println!("Invalid selection");
+        return;
+    }
 
     match op {
         1 => result = x + y,
